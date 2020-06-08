e6b24f6e8bfca6f8e22bd63c893a228cc2a694f1
Evan Crew <crewe@google.com>
2020-06-08 16:52:53 -0400

    Add basic test for example Pynini FST.

--- starter_project/normalize_breton_test.py
+++ starter_project/normalize_breton_test.py
@@ -1,9 +1,15 @@
 import unittest
+import normalize_breton_lib
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_normalize_breton(self):
+        'Test the output of NormalizeBreton.'
+        test_cases = [(('a--bc', 'a-bc'), ('ccb--a', 'ccb-a'), ('ba--aa', 'ba-aa'))]
+        for test in test_cases:
+          for test_case, expected in test:
+            test_fst = normalize_breton_lib.NormalizeBreton(test_case)
+            self.assertEqual(test_fst, expected)
 
 if __name__ == '__main__':
     unittest.main()
