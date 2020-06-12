6bb7d6ef6b9aaa9fce18467b46ae667a673f7253
Andrew Zupon <azupon@google.com>
2020-06-12 00:11:18 +0000

    fleshed out tests for soft, hard, and spirant mutations

--- starter_project/normalize_breton_lib.py
+++ starter_project/normalize_breton_lib.py
@@ -3,25 +3,26 @@
 
 from pynini import *
 
-#BR_GRAPHEMES = union("a", "b", "ch", "c'h", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "y", "z", "D", "G", "K", "'")
-BR_GRAPHEMES = union("d", "a", "p", "e", "n", "b", " ")
-#SOFT_TRIGGERS = union("da", "dre", "a", "war", "dindan", "eme", "en ur", "pe", "ne", "na", "ez", "ra", "en em", "daou", "div", "pa", "holl", "re", "hini")
-SOFT_TRIGGERS = "da"
-HARD_TRIGGERS = union("ho", "az", "ez", "d'az")
-SPIRANT_TRIGGERS = union("he", "ma", "va", "tri", "teir", "pevar", "peder", "nav")
+BR_GRAPHEMES = union("a", "b", "ch", "c'h", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "y", "z", "c", "'", " ")
+SOFT_TRIGGERS = union("da", "dre", "a", "war", "dindan", "eme", "en ur", "pe", "ne", "na", "ez", "ra", "en em", "daou", "div", "pa", "holl", "re", "hini")
+HARD_TRIGGERS = union("ho", "az", "ez", "da'z")
+SPIRANT_TRIGGERS = union("he", "ma", "va", "tri", "teir", "pevar", "peder", "nav", "hon")
 # Skipping mixed triggers for now, since they are all homographs of triggers
 # that trigger other mutations (e.g. "o", "e", and "ma")
-#MIXED_TRIGGERS = union("")
+#MIXED_TRIGGERS = union("o", "e", "ma")
 
 HYPHEN = "-"
-#sigma_star = union(BR_GRAPHEMES, SOFT_TRIGGERS, HARD_TRIGGERS, SPIRANT_TRIGGERS, HYPHEN).closure()
-sigma_star = union(BR_GRAPHEMES, SOFT_TRIGGERS, HYPHEN).closure()
+sigma_star = union(BR_GRAPHEMES, SOFT_TRIGGERS, HARD_TRIGGERS, SPIRANT_TRIGGERS, HYPHEN).closure()
 
 DELETE_HYPHEN = transducer(HYPHEN, "")
-#SOFT_MUTATION = string_map((("p", "b"), ("t", "d"), ("k", "g"), ("gw", "w"), ("m", "v")))
-SOFT_MUTATION = transducer("p", "b")
-#HARD_MUTATION = string_map((("b", "p"), ("d", "t"), ("g", "k"), ("gw", "kw")))
-#SPIRANT_MUTATION = string_map((("p", "f"), ("t", "z"), ("k", "c'h")))
+SOFT_MUTATION = string_map((("p", "b"), ("t", "d"), ("k", "g"), ("gw", "w"), ("m", "v")))
+HARD_MUTATION = union(
+  transducer("b", "p"),
+  transducer("d", "t"),
+  transducer("g", "k"),
+  transducer("gw", "kw", -1),
+  transducer("m", "v"))
+SPIRANT_MUTATION = string_map((("p", "f"), ("t", "z"), ("k", "c'h")))
 # Skipping mixed mutation for now, since all mixed triggers are homographs of
 # triggers that trigger another mutation
 #MIXED_MUTATION = string_map(("b", "v"), ("d", "t"), ("g", "c'h"), ("gw", "w"), ("m", "v"))
@@ -29,6 +30,18 @@ SOFT_MUTATION = transducer("p", "b")
 DO_SOFT_MUTATION = cdrewrite(
     SOFT_MUTATION,
     SOFT_TRIGGERS + " ",
+    BR_GRAPHEMES,
+    sigma_star)
+
+DO_HARD_MUTATION = cdrewrite(
+    HARD_MUTATION,
+    HARD_TRIGGERS + " ",
+    BR_GRAPHEMES,
+    sigma_star)
+
+DO_SPIRANT_MUTATION = cdrewrite(
+    SPIRANT_MUTATION,
+    SPIRANT_TRIGGERS + " ",
     BR_GRAPHEMES,
     sigma_star)
 
@@ -43,3 +56,10 @@ def NormalizeBreton(breton_string):
 
 def NormalizeBretonSoftMutation(breton_string):
   return compose(breton_string, DO_SOFT_MUTATION).string()
+
+def NormalizeBretonHardMutation(breton_string):
+  return compose(breton_string, DO_HARD_MUTATION).string()
+
+def NormalizeBretonSpirantMutation(breton_string):
+  return compose(breton_string, DO_SPIRANT_MUTATION).string()
+
--- starter_project/normalize_breton_test.py
+++ starter_project/normalize_breton_test.py
@@ -14,28 +14,30 @@ class TestStringMethods(unittest.TestCase):
 
     def test_normalize_breton_soft_mutation(self):
         'Test the output of NormalizeBretonSoftMutation.'
-#        test_cases = [(("div plac'h", "div blac'h"), ('da t', 'da d'), ('da Kemper', 'da Gemper'), ("da gwin", "da win"), ('pe mamm', 'pe vamm'))]
-        test_cases = [(("da penn", "da benn"), ("da penn", "da benn"))]
+        test_cases = [(("div plac'h", "div blac'h"), ("da tra", "da dra"), ("da kemper", "da gemper"), ("da gwin", "da win"), ("pe mamm", "pe vamm"))]
+        # see what's going on with capital letters, Kemper and Gemper should be
+        # capitalized
         for test in test_cases:
           for test_case, expected in test:
             test_fst = normalize_breton_lib.NormalizeBretonSoftMutation(test_case)
             self.assertEqual(test_fst, expected)
 
-#    def test_normalize_breton_hard_mutation(self):
-#        'Test the output of NormalizeBretonHardMutation.'
-#        test_cases = [(("da'z bag", "da'z pag"), ('ez douarn', 'ez touarn'), ('ho geriadur', 'ho keriadur'), ("ho Gwenn-ha-Du", "ho Kwenn-ha-du"))]
-#        for test in test_cases:
-#          for test_case, expected in test:
-#            test_fst = normalize_breton_lib.NormalizeBreton(test_case)
-#            self.assertEqual(test_fst, expected)
+    def test_normalize_breton_hard_mutation(self):
+        'Test the output of NormalizeBretonHardMutation.'
+        test_cases = [(("da'z bag", "da'z pag"), ('ez douarn', 'ez touarn'), ('ho geriadur', 'ho keriadur'), ("ho gwenn-ha-du", "ho kwenn-ha-du"))]
+        # Gwenn, Du, and Kwenn should be capitalized
+        for test in test_cases:
+          for test_case, expected in test:
+            test_fst = normalize_breton_lib.NormalizeBretonHardMutation(test_case)
+            self.assertEqual(test_fst, expected)
 
-#    def test_normalize_breton_spirant_mutation(self):
-#        'Test the output of NormalizeBretonSpriantMutation.'
-#        test_cases = [(('tri pesk', 'tri fesk'), ('hon tad', 'hon zad'), ('nav ki', "nav c'hi"))]
-#        for test in test_cases:
-#          for test_case, expected in test:
-#            test_fst = normalize_breton_lib.NormalizeBreton(test_case)
-#            self.assertEqual(test_fst, expected)
+    def test_normalize_breton_spirant_mutation(self):
+        'Test the output of NormalizeBretonSpirantMutation.'
+        test_cases = [(('tri pesk', 'tri fesk'), ('hon tad', 'hon zad'), ('nav ki', "nav c'hi"))]
+        for test in test_cases:
+          for test_case, expected in test:
+            test_fst = normalize_breton_lib.NormalizeBretonSpirantMutation(test_case)
+            self.assertEqual(test_fst, expected)
 
 #    AZ: think about how to do mixed mutation, since all the triggers ("o", "e",
 #    and "ma" are homographs with other triggers for different mutations
