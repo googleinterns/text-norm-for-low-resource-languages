5eda00c7c86be3b4fc0936c21e6f0a72761ae5d5
Andrew Zupon <azupon@google.com>
2020-06-24 19:39:34 +0000

    made unit tests parameterized with subTest

--- starter_project/normalize_breton_test.py
+++ starter_project/normalize_breton_test.py
@@ -12,55 +12,64 @@ class TestStringMethods(unittest.TestCase):
         with open("testdata/bre_normalized_sentences.tsv", "r") as test_file:
             test_cases = test_file.readlines()[1:]
         for sentence in test_cases:
-            test_case = sentence.strip().split("\t")[1]
-            expected = sentence.strip().split("\t")[2]
-            test_fst = normalize_breton_lib.normalize_breton(test_case)
-            self.assertEqual(test_fst, expected)
+            with self.subTest(sentence=sentence):
+                test_case = sentence.strip().split("\t")[1]
+                expected = sentence.strip().split("\t")[2]
+                test_fst = normalize_breton_lib.normalize_breton(test_case)
+                self.assertEqual(test_fst, expected)
 
 
     def test_normalize_breton_soft_mutation(self):
         'Test the Breton soft mutation.'
-        test_cases = [(("Div plac'h", "div blac'h"),
-                       ("DA TRA", "da dra"),
-                       ("da Kemper", "da gemper"),
-                       ("da gwin", "da win"),
-                       ("pe mamm", "pe vamm"))]
-        for test in test_cases:
+        for test in [(("Div plac'h", "div blac'h"),
+                      ("DA TRA", "da dra"),
+                      ("da Kemper", "da gemper"),
+                      ("da gwin", "da win"),
+                      ("pe mamm", "pe vamm"))]:
             for test_case, expected in test:
-                normalized_text = normalize_breton_lib.apply_single_mutation(test_case, "soft")
-                self.assertEqual(normalized_text, expected)
+                with self.subTest(test_case=test_case):
+                    normalized_text = (
+                        normalize_breton_lib.apply_single_mutation(
+                            test_case, "soft"))
+                    self.assertEqual(normalized_text, expected)
 
 
     def test_normalize_breton_soft_mutation_no_mutation(self):
         'Test the Breton soft mutation on words that should not mutate'
-        test_cases = [(("bara kozh", "bara kozh"),
-                       ("Bara ha kig", "bara ha kig"))]
-        for test in test_cases:
+        for test in [(("bara kozh", "bara kozh"),
+                      ("Bara ha kig", "bara ha kig"))]:
             for test_case, expected in test:
-                normalized_text = normalize_breton_lib.apply_single_mutation(test_case, "soft")
-                self.assertEqual(normalized_text, expected)
+                with self.subTest(test_case=test_case):
+                    normalized_text = (
+                        normalize_breton_lib.apply_single_mutation(
+                            test_case, "soft"))
+                    self.assertEqual(normalized_text, expected)
 
 
     def test_normalize_breton_hard_mutation(self):
         'Test the Breton hard mutation.'
-        test_cases = [(("da'z bag", "da'z pag"),
-                       ('ho geriadur', 'ho keriadur'),
-                       ("ho Gwenn-ha-Du", "ho kwenn-ha-du"))]
-        for test in test_cases:
+        for test in [(("da'z bag", "da'z pag"),
+                      ('ho geriadur', 'ho keriadur'),
+                      ("ho Gwenn-ha-Du", "ho kwenn-ha-du"))]:
             for test_case, expected in test:
-                normalized_text = normalize_breton_lib.apply_single_mutation(test_case, "hard")
-                self.assertEqual(normalized_text, expected)
+                with self.subTest(test_case=test_case):
+                    normalized_text = (
+                        normalize_breton_lib.apply_single_mutation(
+                            test_case, "hard"))
+                    self.assertEqual(normalized_text, expected)
 
 
     def test_normalize_breton_spirant_mutation(self):
         'Test the Breton spirant mutation'
-        test_cases = [(('tri pesk', 'tri fesk'),
-                       ('Hon tad', 'hon zad'),
-                       ('nav ki', "nav c'hi"))]
-        for test in test_cases:
+        for test in [(('tri pesk', 'tri fesk'),
+                      ('Hon tad', 'hon zad'),
+                      ('nav ki', "nav c'hi"))]:
             for test_case, expected in test:
-                normalized_text = normalize_breton_lib.apply_single_mutation(test_case, "spirant")
-                self.assertEqual(normalized_text, expected)
+                with self.subTest(test_case=test_case):
+                    normalized_text = (
+                        normalize_breton_lib.apply_single_mutation(
+                            test_case, "spirant"))
+                    self.assertEqual(normalized_text, expected)
 
 
 if __name__ == '__main__':
