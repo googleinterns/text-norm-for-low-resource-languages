379395006554f35c9016cc7afde9aba29b795c01
Andrew Zupon <azupon@google.com>
2020-06-29 23:03:25 +0000

    added normalizations steps 1, 3, and 5 from paper

--- normalizer_lib.py
+++ normalizer_lib.py
@@ -2,6 +2,7 @@
 """Library of language-agnostic FST rewrite rules to normalize text."""
 
 from pynini import *
+import unicodedata
 
 # temporary placeholder, will load language-specific graphemes from config
 GRAPHEMES = union("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"
@@ -10,6 +11,8 @@ GRAPHEMES = union("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"
                   "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
                   "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
                   "y", "z")
+
+PUNCTUATION = union(".", ",", "'", '"', "?", "!")
 
 SPACE = acceptor(" ")
 
@@ -28,6 +31,37 @@ DO_REMOVE_EXTRA_WHITESPACE = cdrewrite(
     SIGMA_STAR)
 
 
+# Step 2: Language-specific formatting fixes
+# skipping for now!
+
+
+# Step 3: Lowercase all characters in languages with case
+# e.g. "isiZulu" -> "isizulu"
+
+
+def unicode_normalize(string: str) -> Fst:
+    "Normalizes unicode to NFC normalization."
+    return transducer(string, unicodedata.normalize("NFC", string))
+
+
+# Step 4: Detach punctuation from words
+# e.g. "Who are you?" -> "Who are you ?"
+
+SEPARATE_PUNCTUATION = transducer("", SPACE)
+
+DO_SEPARATE_PUNCTUATION = (cdrewrite(
+                                    SEPARATE_PUNCTUATION,
+                                    GRAPHEMES,
+                                    PUNCTUATION,
+                                    SIGMA_STAR) @
+                           cdrewrite(
+                                    SEPARATE_PUNCTUATION,
+                                    PUNCTUATION,
+                                    GRAPHEMES,
+                                    SIGMA_STAR))
+
+
 def normalize_everything(string: str) -> str:
     "Apply language-agnostic rewrite rules."
-    return (string @ DO_REMOVE_EXTRA_WHITESPACE).string()
+    string = string.lower()
+    return (string @ unicode_normalize(string) @ DO_REMOVE_EXTRA_WHITESPACE @ DO_SEPARATE_PUNCTUATION).string()
