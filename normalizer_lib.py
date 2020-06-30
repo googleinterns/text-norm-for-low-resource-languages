8f2b3083b837f3fe3a09f623e41842b5c5028852
Andrew Zupon <azupon@google.com>
2020-06-30 20:41:58 +0000

    worked on language-agnostic normalizer

--- normalizer_lib.py
+++ normalizer_lib.py
@@ -3,18 +3,22 @@
 
 from pynini import *
 import unicodedata
+from config import *
 
-# temporary placeholder, will load language-specific graphemes from config
-GRAPHEMES = union("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"
-                  "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X",
-                  "Y", "Z",
-                  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-                  "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
-                  "y", "z")
+language = af
 
-PUNCTUATION = union(".", ",", "'", '"', "?", "!")
+GRAPHEMES = language.GRAPHEMES
+
+INITIAL_PUNCTUATION = language.INITIAL_PUNCTUATION
+FINAL_PUNCTUATION = language.FINAL_PUNCTUATION
+
+PUNCTUATION = union(INITIAL_PUNCTUATION, FINAL_PUNCTUATION)
+
+NUMBERS = union("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
 
 SPACE = acceptor(" ")
+
+VALID_WORDS = union(GRAPHEMES.plus, PUNCTUATION.plus, NUMBERS.plus, SPACE.plus)
 
 SIGMA_STAR = union(*("[{}]".format(i) for i in range(1, 256))).optimize().closure()
 
@@ -26,13 +30,14 @@ REMOVE_EXTRA_WHITESPACE = transducer(SPACE.plus, SPACE)
 
 DO_REMOVE_EXTRA_WHITESPACE = cdrewrite(
     REMOVE_EXTRA_WHITESPACE,
-    GRAPHEMES,
-    GRAPHEMES,
+    union(GRAPHEMES, PUNCTUATION),
+    union(GRAPHEMES, PUNCTUATION),
     SIGMA_STAR)
 
 
 # Step 2: Language-specific formatting fixes
-# skipping for now!
+
+DO_LANGUAGE_SPECIFIC_PREPROCESSING = language.LANGUAGE_SPECIFIC_PREPROCESSING
 
 
 # Step 3: Lowercase all characters in languages with case
@@ -41,10 +46,26 @@ DO_REMOVE_EXTRA_WHITESPACE = cdrewrite(
 
 def unicode_normalize(string: str) -> Fst:
     "Normalizes unicode to NFC normalization."
-    return transducer(string, unicodedata.normalize("NFC", string))
+    NORMALIZER = transducer(string, unicodedata.normalize("NFC", string))
+    return cdrewrite(NORMALIZER, "", "", SIGMA_STAR)
 
 
-# Step 4: Detach punctuation from words
+# Step 4: Discard examples not associated with a pronunciation
+# e.g. "how are you today товарищ?" -> "how are you today?"
+
+VALID_WORDS = GRAPHEMES.plus
+NONWORDS = union("т", "о", "в")
+
+DISCARD_NONWORDS = transducer(NONWORDS.plus, "")
+
+DO_DISCARD_NONWORDS = cdrewrite(
+    DISCARD_NONWORDS,
+    "",
+    "",
+    SIGMA_STAR)
+
+
+# Step 5: Detach punctuation from words
 # e.g. "Who are you?" -> "Who are you ?"
 
 SEPARATE_PUNCTUATION = transducer("", SPACE)
@@ -61,7 +82,28 @@ DO_SEPARATE_PUNCTUATION = (cdrewrite(
                                     SIGMA_STAR))
 
 
+# Step 7: Delete freestanding punctuation
+# e.g. "hi there ?" -> "hi there
+
+DELETE_FREESTANDING_PUNCTUATION = transducer(PUNCTUATION, "")
+
+DO_DELETE_FREESTANDING_PUNCTUATION = cdrewrite(
+    DELETE_FREESTANDING_PUNCTUATION,
+    SPACE,
+    "",
+    SIGMA_STAR)
+
+
 def normalize_everything(string: str) -> str:
     "Apply language-agnostic rewrite rules."
     string = string.lower()
-    return (string @ unicode_normalize(string) @ DO_REMOVE_EXTRA_WHITESPACE @ DO_SEPARATE_PUNCTUATION).string()
+    return (string
+#            @ DO_REMOVE_EXTRA_WHITESPACE
+            @ DO_LANGUAGE_SPECIFIC_PREPROCESSING
+            @ unicode_normalize(string)
+            @ DO_DISCARD_NONWORDS
+#            @ VALID_WORDS
+            @ DO_SEPARATE_PUNCTUATION
+            @ DO_DELETE_FREESTANDING_PUNCTUATION
+            @ DO_REMOVE_EXTRA_WHITESPACE
+            ).string()
