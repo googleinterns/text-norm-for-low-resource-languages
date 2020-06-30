59d1a90e35d1e70015fea7e3cac25992bf16f09d
Andrew Zupon <azupon@google.com>
2020-06-30 20:54:51 +0000

    add rule to keep hyphenated classifiers attached

--- config/zu.py
+++ config/zu.py
@@ -11,3 +11,34 @@ GRAPHEMES = union("'", "-",
 INITIAL_PUNCTUATION = union('"', "'")
 
 FINAL_PUNCTUATION = union("!", '"', ",", ".", ":", ";", "?")
+
+HYPHEN = acceptor("-")
+SPACE = acceptor(" ")
+
+SIGMA_STAR = union(*("[{}]".format(i) for i in range(1, 256))
+                                ).optimize().closure()
+
+NOUN_CLASSIFIERS = union("umu", "um", "u",
+                         "aba", "ab", "abe", "o",
+                         "umu", "um", "u",
+                         "imi", "im",
+                         "ili", "i",
+                         "ama", "ame",
+                         "isi", "is",
+                         "izi", "iz",
+                         "im", "in", "i",
+                         "izim", "izin",
+                         "ulu", "u",
+                         "ubu", "utsh",
+                         "uku", "uk",
+                         "uku", "uk").optimize().closure()
+
+LOAN_CLASSIFIER = transducer(HYPHEN, "")
+
+DO_LOAN_CLASSIFIER = cdrewrite(
+    LOAN_CLASSIFIER,
+    union("[BOS]", SPACE) + NOUN_CLASSIFIERS,
+    GRAPHEMES,
+    SIGMA_STAR)
+
+LANGUAGE_SPECIFIC_PREPROCESSING = DO_LOAN_CLASSIFIER
