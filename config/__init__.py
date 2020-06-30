49f241222d5a76eac1029de09e3753d4e81eb438
Andrew Zupon <azupon@google.com>
2020-06-30 20:36:50 +0000

    added language-specific configs

---
+++ config/__init__.py
@@ -0,0 +1,13 @@
+#import af
+#import am
+#import bm_latn
+#import ig
+#import ha
+#import mg
+#import so
+#import sw
+#import wo
+#import yo
+#import zu
+__all__ = ["af", "am", "bm_latn", "ig", "ha",
+           "mg", "so", "sw", "wo", "yo", "zu"]
---
+++ config/zu.py
@@ -0,0 +1,13 @@
+from pynini import *
+
+GRAPHEMES = union("'", "-",
+                  "A", "B", "C", "D", "E", "F", "G", "H", "I",
+                  "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+                  "S", "T", "U", "V", "W", "X", "Y", "Z",
+                  "a", "b", "c", "d", "e", "f", "g", "h", "i",
+                  "j", "k", "l", "m", "n", "o", "p", "q", "r",
+                  "s", "t", "u", "v", "w", "x", "y", "z")
+
+INITIAL_PUNCTUATION = union('"', "'")
+
+FINAL_PUNCTUATION = union("!", '"', ",", ".", ":", ";", "?")
