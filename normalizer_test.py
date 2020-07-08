0a67d6e4414ecdcb29e09ec24e70fa321821ca96
Andrew Zupon <azupon@google.com>
2020-07-08 19:57:47 +0000

    addressed PR changes on normalizer_test.py

--- normalizer_test.py
+++ normalizer_test.py
@@ -4,19 +4,19 @@
 import unittest
 import normalizer_lib
 
-class TestStringMethods(unittest.TestCase):
+class TestNormalizer(unittest.TestCase):
     """Tests for evaluating text normalizer."""
 
 
-    def test_normalize_everything(self):
-        'Test the output of normalize_everything.'
+    def test_normalizer(self):
+        'Test the output of normalizer.'
         with open("testdata/normalized_sentences.tsv", "r") as test_file:
             test_cases = test_file.readlines()[1:]
         for sentence in test_cases:
             with self.subTest(sentence=sentence):
                 test_case = sentence.strip().split("\t")[1]
                 expected = sentence.strip().split("\t")[2]
-                test_fst = normalizer_lib.normalize_everything(test_case)
+                test_fst = normalizer_lib.normalizer(test_case)
                 self.assertEqual(test_fst, expected)
 
 
