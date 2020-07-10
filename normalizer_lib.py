1a295b1c1195cf474142a0b2acbfef9640135de7
Andrew Zupon <azupon@google.com>
2020-07-10 20:06:41 +0000

    separate defs for token and sentence filtering
    also deleted unused code and ran pylint

--- normalizer_lib.py
+++ normalizer_lib.py
@@ -2,8 +2,10 @@
 """Library of language-agnostic FST rewrite rules to normalize text."""
 
 import unicodedata
-import conf
+from dataclasses import dataclass
+from typing import List
 from pynini import *
+import conf
 from config import *
 
 
@@ -22,13 +24,11 @@ SIGMA_STAR = union(*("[{}]".format(i) for i in range(1, 256))
                    ).optimize().closure()
 
 
+@dataclass
 class Verbalizable:
+    """Contains verbalizable tokens."""
 
-
-    def __init__(self: str) -> None:
-        self.verbalizable = "None"
-
-
+    @staticmethod
     def verbalizable():
         """Returns union of verbalizable tokens.
 
@@ -36,17 +36,33 @@ class Verbalizable:
 
         returns: Union of acceptors for verbalizable tokens.
         """
-        TOKEN = INITIAL_PUNCTUATION.ques + (GRAPHEMES.plus | NUMBERS.plus).plus + FINAL_PUNCTUATION.ques
+        token = (INITIAL_PUNCTUATION.ques +
+                 (GRAPHEMES.plus | NUMBERS.plus).plus +
+                 FINAL_PUNCTUATION.ques)
 
-        EMAIL_ADDRESS = GRAPHEMES.plus + "@" + GRAPHEMES.plus + closure("." + GRAPHEMES.plus, 1, 2)
+        email_address = (GRAPHEMES.plus +
+                         "@" +
+                         GRAPHEMES.plus +
+                         closure("." + GRAPHEMES.plus, 1, 2))
 
-        WEB_ADDRESS = (("http" + acceptor("s").ques + "://").ques + "www.").ques + union(GRAPHEMES, NUMBERS).plus + closure("." + GRAPHEMES.plus, 1, 2)
+        web_address = ((("http" + acceptor("s").ques + "://").ques +
+                        "www.").ques +
+                       union(GRAPHEMES, NUMBERS).plus +
+                       closure("." + GRAPHEMES.plus, 1, 2))
 
-        TIME = closure(NUMBERS, 1, 2) + closure(":" + closure(NUMBERS, 1, 2), 1, 2)
+        time = (closure(NUMBERS, 1, 2) +
+                closure(":" + closure(NUMBERS, 1, 2), 1, 2))
 
-        FANCY_NUMBERS = closure(NUMBERS, 1, 6) | (closure(NUMBERS, 1,6) + union(",", ".") + closure(NUMBERS, 1, 4)) | ((closure(NUMBERS, 1, 3) + union(",", ".")).ques + closure(NUMBERS, 1, 3) + (union(",", ".") + closure(NUMBERS, 1,4).ques))
+        fancy_numbers = (closure(NUMBERS, 1, 6) |
+                         (closure(NUMBERS, 1, 6)
+                          + union(",", ".") + closure(NUMBERS, 1, 4)) |
+                         ((closure(NUMBERS, 1, 3) +
+                           union(",", ".")).ques +
+                          closure(NUMBERS, 1, 3) +
+                          (union(",", ".") +
+                           closure(NUMBERS, 1, 4).ques)))
 
-        return union(TOKEN, EMAIL_ADDRESS, WEB_ADDRESS, TIME, FANCY_NUMBERS)
+        return union(token, email_address, web_address, time, fancy_numbers)
 
 
 # Step 1: Remove all extra whitespace between words
@@ -63,30 +79,18 @@ REMOVE_EXTRA_WHITESPACE = cdrewrite(
 LANGUAGE_SPECIFIC_NORM = LANGUAGE.LANGUAGE_SPECIFIC_PREPROCESSING
 
 
-# Step 3: Apply NFC unicode normalize
-# really slow when done this way,
-# super fast when I just call unicodedata.normalize
-# on the string at the beginning
-# may remove later
-#def unicode_normalize(string: str) -> Fst:
-#    "Normalizes unicode to NFC normalization."
-#    nfc_normalized = unicodedata.normalize("NFC", string)
-#    normalizer = transducer(string, nfc_normalized).optimize()
-#    return cdrewrite(normalizer, "", "", SIGMA_STAR).optimize()
-
-
 # Step 4: Discard examples not associated with a pronunciation
 # e.g. "how are you today товарищ?" -> "how are you today?"
 
 
 def pass_only_valid_tokens(string: str) -> str:
-    """Replaces invalid tokens in a sentence with <UNK>, keeps the rest.
+    """Replaces invalid strings in a sentence.
 
     Args:
         string: A line from a corpus.
 
     Returns:
-        The line, but where invalid tokens have been replaced by <UNK>.
+        The line, where invalid tokens have been replaced with <UNK>.
     """
     valid_token = INITIAL_PUNCTUATION.ques + Verbalizable.verbalizable() + FINAL_PUNCTUATION.ques
     returned: List[str] = []
@@ -98,6 +102,24 @@ def pass_only_valid_tokens(string: str) -> str:
         else:
             returned.append("<UNK>")
     return " ".join(returned)
+
+
+def pass_only_valid_sentences(string: str) -> str:
+    """Replaces invalid sentences.
+
+    Args:
+        string: A line from a corpus.
+
+    Returns:
+        The line or the line replaced with <SENTENCE_REJECTED>.
+    """
+    valid_token = INITIAL_PUNCTUATION.ques + Verbalizable.verbalizable() + FINAL_PUNCTUATION.ques
+    remove_extra_whitespace = (string @ REMOVE_EXTRA_WHITESPACE).string()
+    split_string = remove_extra_whitespace.split(" ")
+    for token in split_string:
+        if difference(acceptor(token), valid_token).num_states() != 0:
+            return "<SENTENCE_REJECTED>"
+    return string
 
 
 # Step 5: Detach punctuation from words
@@ -112,7 +134,8 @@ SEPARATE_PUNCTUATION = (
     cdrewrite(
         INSERT_SPACE,
         union("[BOS]", SPACE) + union(NON_GRAPHEME_PUNCT),
-        union(PUNCTUATION, Verbalizable.verbalizable()) + union("[EOS]", SPACE, Verbalizable.verbalizable()),
+        (union(PUNCTUATION, Verbalizable.verbalizable()) +
+         union("[EOS]", SPACE, Verbalizable.verbalizable())),
         SIGMA_STAR) @
     cdrewrite(
         INSERT_SPACE,
@@ -133,8 +156,19 @@ DELETE_FREESTANDING_PUNCTUATION = cdrewrite(
     SIGMA_STAR)
 
 
-def normalizer(string: str) -> str:
-    """Applies FST rewrite rules to normalize text.
+def apply_fst_rules(string: str) -> str:
+    """Applies FST rewrite rules."""
+    return string if (string == "<STRING_REJECTED>") else (
+        string
+        @ LANGUAGE_SPECIFIC_NORM
+        @ SEPARATE_PUNCTUATION
+        @ DELETE_FREESTANDING_PUNCTUATION
+        @ REMOVE_EXTRA_WHITESPACE
+        ).optimize().string()
+
+
+def token_based_normalizer(string: str) -> str:
+    """Normalizes text by applying FST rewrite rules.
 
     Args:
         string: A line from a corpus.
@@ -143,16 +177,19 @@ def normalizer(string: str) -> str:
         The normalized line from the corpus.
     """
     string = unicodedata.normalize("NFC", string.lower())
-    #filtered_string = string @ SENTENCE # for all-or-nothing filtering
-    filtered_string = pass_only_valid_tokens(string) # for token-based filtering
-    try:
-        return (filtered_string
-                #@ REMOVE_EXTRA_WHITESPACE
-                @ LANGUAGE_SPECIFIC_NORM
-                #@ unicode_normalize(string) ## SLOW, doing this above instead
-                @ SEPARATE_PUNCTUATION
-                #@ DELETE_FREESTANDING_PUNCTUATION
-                @ REMOVE_EXTRA_WHITESPACE
-                ).optimize().string()
-    except Warning:
-        return "<STRING REJECTED>"
+    filtered_string = pass_only_valid_tokens(string)
+    return apply_fst_rules(filtered_string)
+
+
+def sentence_based_normalizer(string: str) -> str:
+    """Normalizes text by applying FST rewrite rules.
+
+    Args:
+        string: A line from a corpus.
+
+    Returns:
+        The normalized line from the corpus.
+    """
+    string = unicodedata.normalize("NFC", string.lower())
+    filtered_string = pass_only_valid_sentences(string)
+    return apply_fst_rules(filtered_string)
