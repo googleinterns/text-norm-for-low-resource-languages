e243c12d4e3d6f1aa360c42d98b70210ec824259
Andrew Zupon <azupon@google.com>
2020-07-15 21:06:02 +0000

    filled in amharic, hausa, and igbo configs

---
+++ config/ha.py
@@ -0,0 +1,25 @@
+"Hausa config with language-specific information."
+
+from pynini import *
+
+GRAPHEMES = union("'", "-", "ʼ"
+                  "A", "B", "C", "D", "E", "F", "G", "H", "I",
+                  "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+                  "S", "T", "U", "V", "W", "X", "Y", "Z",
+                  "a", "b", "c", "d", "e", "f", "g", "h", "i",
+                  "j", "k", "l", "m", "n", "o", "p", "q", "r",
+                  "s", "t", "u", "v", "w", "x", "y", "z",
+                  "Ɓ", "Ɗ", "Ƙ", "Ƙ", "R̃",
+                  "ɓ", "ɗ", "ƙ", "ƴ", "r̃")
+
+INITIAL_PUNCTUATION = union('"', "'")
+
+FINAL_PUNCTUATION = union("!", '"', ",", ".", ":", ";", "?")
+
+NUMBERS = union("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+
+UD = ""
+UM = ""
+AC = ""
+OSCAR = ""
+LCC = ""
