62f9270f50a852f14a13e956537987fc5d2164c4
Andrew Zupon <azupon@google.com>
2020-07-21 19:20:55 +0000

    edited tests to work with new class

--- normalizer_test.py
+++ normalizer_test.py
@@ -4,9 +4,12 @@
 import unittest
 import normalizer_lib
 
+norm = normalizer_lib.NormalizerLib("af")
+
 class TestNormalizer(unittest.TestCase):
     """Tests for evaluating text normalizer."""
 
+    #norm = normalizer_lib.NormalizerLib("af")
 
     def test_token_normalizer(self):
         'Test the output of normalizer.'
@@ -42,9 +45,10 @@ class TestNormalizer(unittest.TestCase):
                       ("   all   the   spaces   ", " all the spaces "))]:
             for test_case, expected in test:
                 with self.subTest(test_case=test_case):
-                    normalized_text = (test_case @
-                                       normalizer_lib.REMOVE_EXTRA_WHITESPACE
-                                       ).string()
+                    normalized_text = norm.remove_extra_whitespace(test_case).string()
+#                    normalized_text = (test_case @
+#                                       normalizer_lib.REMOVE_EXTRA_WHITESPACE
+#                                       ).string()
                     self.assertEqual(normalized_text, expected)
 
 
@@ -54,23 +58,27 @@ class TestNormalizer(unittest.TestCase):
                        "hello , friend"),
                       ("the end.",
                        "the end ."),
-                      ('"What',
-                       '" What'),
-                      ('"Who, he asked, left?"',
-                       '" Who , he asked , left ? "'),
-                      ("Don't separate apostrophes",
-                       "Don't separate apostrophes"),
+                      ('"what',
+                       '" what'),
+                      ('"who, he asked, left?"',
+                       '" who , he asked , left ? "'),
+                      ("don't separate apostrophes",
+                       "don't separate apostrophes"),
                       ("initial 'apostrophe",
                        "initial 'apostrophe"),
                       ("final' apostrophe",
                        "final ' apostrophe"),
-                      ("Keep ice-cream together",
-                       "Keep ice-cream together"))]:
+                      ("keep ice-cream together",
+                       "keep ice-cream together"),
+                      ("50,000", "50,000"),
+                      ("google.com", "google.com"),
+                      ("12:25", "12:25"))]:
             for test_case, expected in test:
                 with self.subTest(test_case=test_case):
-                    normalized_text = (test_case @
-                                       normalizer_lib.SEPARATE_PUNCTUATION
-                                       ).string()
+                    normalized_text = norm.detach_punctuation(test_case).string()
+#                    normalized_text = (test_case @
+#                                       normalizer_lib.SEPARATE_PUNCTUATION
+#                                       ).string()
                     self.assertEqual(normalized_text, expected)
 
 
@@ -78,13 +86,14 @@ class TestNormalizer(unittest.TestCase):
         'Test deleting freestanding punctuation.'
         for test in [(("hello , friend", "hello  friend"),
                       ("the end .", "the end "),
-                      ('" What', ' What'),
-                      ('" Who , he asked , left ? "', ' Who  he asked  left  '))]:
+                      ('" what', ' what'),
+                      ('" who , he asked , left ? "', ' who  he asked  left  '))]:
             for test_case, expected in test:
                 with self.subTest(test_case=test_case):
-                    normalized_text = (test_case @
-                                       normalizer_lib.DELETE_FREESTANDING_PUNCTUATION
-                                       ).string()
+                    normalized_text = norm.delete_freestanding_punctuation(test_case).string()
+#                    normalized_text = (test_case @
+#                                       normalizer_lib.DELETE_FREESTANDING_PUNCTUATION
+#                                       ).string()
                     self.assertEqual(normalized_text, expected)
 
 
@@ -95,7 +104,8 @@ class TestNormalizer(unittest.TestCase):
                       ("Где мой dog?", "<UNK> <UNK> dog?"))]:
             for test_case, expected in test:
                 with self.subTest(test_case=test_case):
-                    normalized_text = normalizer_lib.pass_only_valid_tokens(test_case)
+                    normalized_text = norm.pass_only_valid_tokens(test_case)
+#                    normalized_text = normalizer_lib.pass_only_valid_tokens(test_case)
                     self.assertEqual(normalized_text, expected)
 
 if __name__ == '__main__':
