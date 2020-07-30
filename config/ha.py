1da4e7adcf9727b867c1af972f1a0c71b60b0f28
Andrew Zupon <azupon@google.com>
2020-07-30 19:26:12 +0000

    added ha rules for different <y> normalizations

--- config/ha.py
+++ config/ha.py
@@ -4,7 +4,7 @@ from pynini import *
 from pynini.lib import byte
 from config import utils
 
-GRAPHEMES = union(byte.LOWER, "'", "-", "ʼ"
+GRAPHEMES = union(byte.LOWER, "'", "-", "ʼ", "ʼ"
                   "ɓ", "ɗ", "ƙ", "ƴ", "r̃")
 
 INITIAL_PUNCTUATION = utils.DEFAULT_INITIAL_PUNCTUATION
@@ -12,6 +12,22 @@ INITIAL_PUNCTUATION = utils.DEFAULT_INITIAL_PUNCTUATION
 FINAL_PUNCTUATION = utils.DEFAULT_FINAL_PUNCTUATION
 
 NUMERALS = byte.DIGIT
+
+
+APOSTROPHE_TO_HOOK_Y = cdrewrite(
+    cross("ʼy", "ƴ"),
+    "",
+    "",
+    byte.BYTES.closure())
+
+HOOK_TO_APOSTROPHE_Y = cdrewrite(
+    cross("ƴ", "ʼy"),
+    "",
+    "",
+    byte.BYTES.closure())
+
+#LANGUAGE_SPECIFIC_PREPROCESSING = APOSTROPHE_TO_HOOK_Y
+LANGUAGE_SPECIFIC_PREPROCESSING = HOOK_TO_APOSTROPHE_Y
 
 UD = ""
 UM = ""
